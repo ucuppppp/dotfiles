ec84266ee5ab81c62726a42a1f32d2c4f962429e
ucuppppp <radithamzah12@gmail.com>
2025-11-01 00:56:23 +0700

    tmux and nvim theme update

--- .config/nvim/lua/ucup/remap.lua
+++ .config/nvim/lua/ucup/remap.lua
@@ -3,25 +3,27 @@ vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>nh", vim.cmd.nohl)
 
 if vim.g.vscode then
-	-- VSCode Neovim
-	require("ucup.vscode_remap")
+  -- VSCode Neovim
+  require("ucup.vscode_remap")
 else
-	-- Ordinary Neovim
-	vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-	vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+  vim.keymap.set("n", "<leader>r, ", ":source %<CR>")
 
-	-- enter visual mode then yank the line
-	vim.keymap.set("n", "<leader>c", "V:y<CR>")
+  -- Ordinary Neovim
+  vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+  vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-	-- yank to clipboard
-	vim.keymap.set("v", "<leader>y", '"+y')
+  -- enter visual mode then yank the line
+  vim.keymap.set("n", "<leader>c", "V:y<CR>")
 
-	-- tabpage
-	-- vim.keymap.set({ "n", "v" }, "<leader>pn", ":tabnext<CR>")
-	-- vim.keymap.set({ "n", "v" }, "<leader>pp", ":tabprev<CR>")
-	-- vim.keymap.set({ "n", "v" }, "<leader>pc", ":tabnew<CR>")
+  -- yank to clipboard
+  vim.keymap.set("v", "<leader>y", '"+y')
 
-	-- buffer next/prev
-	vim.keymap.set("n", "<leader>pn", ":bnext<CR>")
-	vim.keymap.set("n", "<leader>pb", ":bprev<CR>")
+  -- tabpage
+  -- vim.keymap.set({ "n", "v" }, "<leader>pn", ":tabnext<CR>")
+  -- vim.keymap.set({ "n", "v" }, "<leader>pp", ":tabprev<CR>")
+  -- vim.keymap.set({ "n", "v" }, "<leader>pc", ":tabnew<CR>")
+
+  -- buffer next/prev
+  vim.keymap.set("n", "<leader>pn", ":bnext<CR>")
+  vim.keymap.set("n", "<leader>pb", ":bprev<CR>")
 end
